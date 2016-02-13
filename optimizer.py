e37d6d9b59a2853d41fddede7b0f5faf3c408a88
Abhinav <abhinav181@gmail.com>
2016-02-12 19:41:02 -0800

    hw2

--- optimizer.py
+++ optimizer.py
@@ -10,17 +10,18 @@ import math
 import copy
 import sys
 
-def simulate(startdate, enddate, symbols, percentage):
+
+def get_data(startdate, enddate, symbols):
   dt_timeofday = dt.timedelta(hours=16)
   ldt_timestamps = du.getNYSEdays(startdate, enddate, dt_timeofday)
-
   c_dataobj = da.DataAccess('Yahoo', cachestalltime=0)
   ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
   ldf_data = c_dataobj.get_data(ldt_timestamps, symbols, ls_keys)
   d_data = dict(zip(ls_keys, ldf_data))
-
-
   normalized_vals = d_data['close'].values / d_data['close'].values[0, :]
+  return normalized_vals
+
+def simulate(normalized_vals, percentage):
   indiv_daily_value = percentage*normalized_vals
   total_dv = indiv_daily_value.sum(axis=1)
   
@@ -49,7 +50,7 @@ def get_percentages(inp, index, percentages):
     return
 
   i = 0.0
-  while (i <= 1.0):
+  while (i < 1.1):
     inp[index] = i
     get_percentages(inp, index+1, percentages)
     i = i + 0.1
@@ -59,20 +60,23 @@ def best_portfolio(dt_start, dt_end, ls_symbols):
   get_percentages(np.zeros(len(ls_symbols)), 0, percentages)
 
   print str(len(percentages))
-  raw_input()
 
+  normalized_vals = get_data(dt_start, dt_end, ls_symbols)
+  print normalized_vals
+  raw_input()
+  
   bests = None
   bestp = None
   for p in percentages:
-    v, ar, s, cr = simulate(dt_start, dt_end, ls_symbols, p)
+    v, ar, s, cr = simulate(normalized_vals, p)
+    print "P: %s\tS: %s\tbests: %s\tbestp: %s\tar: %s" % (str(p), str(s), str(bests), str(bestp), str(ar)) 
 
     if bests == None:
       bests = s
-      bestp = p 
+      bestp = p
    
-    if s == bests:
-      print "sharpe: "+str(s)
-      print "percentage: "+str(p)
+    if abs(s-bests)<0.00000001:
+      print "SAME: P: %s\tS: %s" % (str(p), str(s))
 
     if s > bests:
       bests = s
@@ -93,7 +97,7 @@ def main():
   print "Best Allocation"
   print best_allocation
 
-  volatility, average_d, sharpe, cumm_ret = simulate(dt_start, dt_end, ls_symbols, best_allocation)
+  volatility, average_d, sharpe, cumm_ret = simulate(get_data(dt_start, dt_end, ls_symbols), best_allocation)
   print "Sharpe: "+str(sharpe)
   print "Volatility: "+str(volatility)
   print "Average Daily Return: "+str(average_d)
