9cb472b15c89d50472768c83dc72366f85661982
Abhinav <abhinav181@gmail.com>
2016-02-15 12:06:09 -0800

    hw2

--- hw2.py
+++ hw2.py
@@ -25,12 +25,12 @@ nan = no information about any event.
 """
 
 
-def find_events(ls_symbols, d_data):
+def find_events(ls_symbols, d_data, p):
     ''' Finding the event dataframe '''
     df_close = d_data['actual_close']
     ts_market = df_close['SPY']
 
-    print "Finding Events"
+    print "Finding Events - %f" % p
 
     # Creating an empty dataframe
     df_events = copy.deepcopy(df_close)
@@ -51,32 +51,35 @@ def find_events(ls_symbols, d_data):
 
             # Event is found if the symbol is down more then 3% while the
             # market is up more then 2%
-            if f_symprice_today < 5.0 and f_symprice_yest >= 5.0:
+            if f_symprice_today < p and f_symprice_yest >= p:
                 df_events[s_sym].ix[ldt_timestamps[i]] = 1
 
     return df_events
 
 
 if __name__ == '__main__':
-    dt_start = dt.datetime(2008, 1, 1)
-    dt_end = dt.datetime(2009, 12, 31)
-    ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt.timedelta(hours=16))
+    for data in ['sp5002008', 'sp5002012']:
+        dt_start = dt.datetime(2008, 1, 1)
+        dt_end = dt.datetime(2009, 12, 31)
+        ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt.timedelta(hours=16))
 
-    dataobj = da.DataAccess('Yahoo')
-    ls_symbols = dataobj.get_symbols_from_list('sp5002008')
-    ls_symbols.append('SPY')
+        dataobj = da.DataAccess('Yahoo')
+        ls_symbols = dataobj.get_symbols_from_list(data)
+        ls_symbols.append('SPY')
 
-    ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
-    ldf_data = dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
-    d_data = dict(zip(ls_keys, ldf_data))
+        ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
+        ldf_data = dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
+        d_data = dict(zip(ls_keys, ldf_data))
 
-    for s_key in ls_keys:
-        d_data[s_key] = d_data[s_key].fillna(method='ffill')
-        d_data[s_key] = d_data[s_key].fillna(method='bfill')
-        d_data[s_key] = d_data[s_key].fillna(1.0)
+        for s_key in ls_keys:
+            d_data[s_key] = d_data[s_key].fillna(method='ffill')
+            d_data[s_key] = d_data[s_key].fillna(method='bfill')
+            d_data[s_key] = d_data[s_key].fillna(1.0)
 
-    df_events = find_events(ls_symbols, d_data)
-    print "Creating Study"
-    ep.eventprofiler(df_events, d_data, i_lookback=20, i_lookforward=20,
-                s_filename='MyEventStudy.pdf', b_market_neutral=True, b_errorbars=True,
+        for price in [5.0, 6.0, 7.0, 8.0, 9.0, 10.0]:
+            df_events = find_events(ls_symbols, d_data, price)
+            print "Creating Study data - %s, price - %f" % (data, price)
+            filename = "EventStudy-%s-%s.pdf" % (data, str(price))
+            ep.eventprofiler(df_events, d_data, i_lookback=20, i_lookforward=20,
+                s_filename=filename, b_market_neutral=True, b_errorbars=True,
                 s_market_sym='SPY')
