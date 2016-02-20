5cfb23925c1e1ee658d2578866082a3f46bd2ab1
Abhinav <abhinav181@gmail.com>
2016-02-20 10:28:01 -0800

    b

--- marketsym.py
+++ marketsym.py
@@ -12,16 +12,34 @@ import QSTK.qstkstudy.EventProfiler as ep
 
 
 def main():
-  cash = sys.argv[1]
-  orders_csv = None
-  with open(sys.argv[2]) as f:
-  	orders_csv = f.read()
+	cash = sys.argv[1]
+	orders_csv = None
+	with open(sys.argv[2]) as f:
+		orders_csv = f.read()
 
-  orders_csv_reader = csv.DictReader(orders_csv.splitlines(), fieldnames=('year', 'month' , 'day', 'symbol', 'action', 'units'))
-  orders = list()
-  for row in orders_csv_reader:
-    row['datetime'] = dt.datetime(int(row['year']), int(row['month']), int(row['day']))
-    orders.append(row)
+	orders_csv_reader = csv.DictReader(orders_csv.splitlines(), fieldnames=('year', 'month' , 'day', 'symbol', 'action', 'units'))
+
+	orders = list()
+	for row in orders_csv_reader:
+		row['datetime'] = dt.datetime(int(row['year']), int(row['month']), int(row['day']))
+		orders.append(row)
+
+	# generate date ranges
+	ldt_timestamps = du.getNYSEdays(orders[0]['datetime'], orders[-1]['datetime'], dt.timedelta(hours=16))
+
+	# get prices
+	dataobj = da.DataAccess('Yahoo')
+	symbols = set()
+	for row in orders:
+		symbols.add(row['symbol'])
+
+	dataobj = da.DataAccess('Yahoo')
+	ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
+	ldf_data = dataobj.get_data(ldt_timestamps, list(symbols), ls_keys)
+	d_data = dict(zip(ls_keys, ldf_data))
+
+	
+
 
 if __name__ == '__main__':
 	main()
